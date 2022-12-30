26f7e4c1217f7675acca3d0923f1d62626134da3
DevCheesard <chizee200@gmail.com>
2022-12-30 20:15:04 +0100

    final commit. Run code and review

--- AnimalKingdom.js
+++ AnimalKingdom.js
@@ -1,128 +1,179 @@
-//This is the abstract class from which subclasses will inherit
+//This is the base class from which subclasses will inherit
 //The KingdomAnimalia class shows the attributes (name, species, diet, and habitat) and behaviors (move, eat, and reproduce) of an animal in a single place.
+//This shows abstraction
 class KingdomAnimalia {
+    // The constructor function is used to create and initialize an object created from a class
     constructor(name, species, diet, habitat) {
-        if(this.constructor === KingdomAnimalia){
-            throw new Error (' this Class cannot be instantiated.');
+        if (this.constructor === KingdomAnimalia) {
+            throw new Error(' this Class cannot be instantiated.');
         }
-      this.name = name;
-      this.species = species;
-      this.diet = diet;
-      this.habitat = habitat;
+        this.name = name;
+        this.species = species;
+        this.diet = diet;
+        this.habitat = habitat;
     }
-  
+
     move() {
-      console.log(`${this.name} is moving.`);
-    }
-  
+        console.log(`${this._name} is moving.`);
+        this._move();
+      }
+      
+      _move() {
+        // Implementation details of the actual movement of the animal. 
+        //The user can then call the move() method without knowing the details of how the movement is actually implemented.
+        //This shows encapsulation
+      }
+
     eat() {
-      console.log(`${this.name} is eating ${this.diet}.`);
+        console.log(`${this.name} is eating ${this.diet}.`);
     }
-  
+
     reproduce() {
-      console.log(`${this.name} is reproducing.`);
-    }
-  }
-  
-
-  // These subclasses (HasBackbone and Nobackbone) inherit from the Parent class. 
-  class HasBackbone extends KingdomAnimalia {
-    constructor(name){
-        super (name);
-    if(this.constructor === HasBackbone){
-        throw new Error (' this Class cannot be instantiated.');
-    }
-
-    this.backbone = ('have a backbone');
+        console.log(`${this.name} is reproducing.`);
     }
 }
 
-class NoBackbone extends KingdomAnimalia {
-    constructor(name){
-        super (name);
-    if(this.constructor === NoBackbone){
-        throw new Error ('this Class cannot be instantiated.');
-    }
-    
-    this.backbone = ('have no backbone');
+
+// These subclasses (Vertebrates and Invertebrates) inherit from the Parent class. 
+// The `extends` keyword is used to create classes that are subclasses of a parent class. This shows inheritance
+class Vertebrates extends KingdomAnimalia {
+    constructor(name) {
+        // The `super` keyword is used to call the constructor of the parent class
+        super(name);
+        if (this.constructor === Vertebrates) {
+            throw new Error(' this Class cannot be instantiated.');
+        }
+
+        this.backbone = ('have a backbone');
     }
 }
-  class Mammal extends HasBackbone {
+
+class Invertebrates extends KingdomAnimalia {
+    constructor(name) {
+        super(name);
+        if (this.constructor === Invertebrates) {
+            throw new Error('this Class cannot be instantiated.');
+        }
+
+        this.backbone = ('have no backbone');
+    }
+}
+
+//Using abstraction to hide the implementation details of certain behaviors and only expose the necessary interface to the user. For example:
+class Mammal extends Vertebrates {
     constructor(name, species, diet, habitat, numLegs) {
-      super(name, species, diet, habitat);
-      this.numLegs = numLegs;
+        super(name, species, diet, habitat);
+        this.name = name;
+        this.species = species;
+        this.diet = diet;
+        this.habitat = habitat;
+        this.numLegs = numLegs;
+        this.bodytemp= ('warm blooded');
     }
-  
+
     giveBirth() {
-      console.log(`${this.name} is giving birth.`);
+        console.log(`${this.name} is giving birth.`);
     }
-  }
-  
-  class Fish extends NoBackbone {
+}
+// Instantiating object from the subclass above.
+const cat = new Mammal('Cat', 'felidae', 'canned food', 'land','4');
+
+class Fish extends Invertebrates {
     constructor(name, species, diet, habitat, numFins) {
-      super(name, species, diet, habitat);
-      this.numFins = numFins;
+        super(name, species, diet, habitat);
+        this.name = name;
+        this.species = species;
+        this.diet = diet;
+        this.habitat = habitat;
+        this.numFins = numFins;
+        this.bodytemp= ('cold blooded');
     }
-  
+
     swim() {
-      console.log(`${this.name} is swimming.`);
+        console.log(`${this.name} is swimming.`);
     }
-  }
- 
-  //The Aves class overrides the move behavior inherited from the KingdomAnmalia class to specify that birds fly rather than move in a general sense.
-  // This shows polymorphism
-  class Aves extends HasBackbone {
+}
+// Instantiating Object from the subclass above.
+const tilapia = new Fish('Tilapia','cichlidae','plankton','freshwater','7');
+
+//The Aves class overrides the move behavior inherited from the KingdomAnmalia class to specify that birds fly rather than move in a general sense.
+// This shows polymorphism
+class Aves extends Vertebrates {
     constructor(name, species, diet, habitat, numWings) {
-      super(name, species, diet, habitat);
-      this.numWings = numWings;
+        super(name, species, diet, habitat);
+        this.name = name;
+        this.species = species;
+        this.diet = diet;
+        this.habitat = habitat;
+        this.numWings = numWings;
+        this.bodytemp= ('warm blooded');
     }
-  
+
     move() {
-      console.log(`${this.name} is flying.`);
+        console.log(`${this.name} is flying.`);
     }
-  }
+}
+// Instantiating object from the subclass above.
+const dove = new Aves('Dove','columbidae','grains','arboreal','2');
 
-  class Amphibia extends HasBackbone {
+
+class Amphibia extends Vertebrates {
     constructor(name, species, diet, habitat, numLegs) {
-      super(name, species, diet, habitat);
-      this.numLegs = numLegs;
+        super(name, species, diet, habitat);
+        this.name = name;
+        this.species = species;
+        this.diet = diet;
+        this.habitat = habitat;
+        this.numLegs = numLegs;
+        this.bodytemp= ('cold blooded');
     }
-    
+
     swim() {
-      console.log(`${this.name} is swimming.`);
+        console.log(`${this.name} is swimming.`);
     }
-    
+
     crawl() {
-      console.log(`${this.name} is crawling.`);
+        console.log(`${this.name} is crawling.`);
     }
-  }
+}
+// Instantiating object from the subclass above.
+const toad = new Amphibia('Toad','bufonidae','insects','moist land','4');
 
-  class Arthropoda extends NoBackbone {
+class Arthropoda extends Invertebrates {
     constructor(name, species, diet, habitat, numLegs) {
-      super(name, species, diet, habitat);
-      this.numLegs = numLegs;
+        super(name, species, diet, habitat);
+        this.name = name;
+        this.species = species;
+        this.diet = diet;
+        this.habitat = habitat;
+        this.numLegs = numLegs;
+        this.bodytemp= ('cold blooded');
     }
-    
+
     crawl() {
-      console.log(`${this.name} is crawling.`);
+        console.log(`${this.name} is crawling.`);
     }
-  }
-  //Using abstraction to hide the implementation details of certain behaviors and only expose the necessary interface to the user. For example:
-  
-  class Reptile extends HasBackbone {
+}
+// Instantiating object from the subclass above.
+const butterfly = new Arthropoda('Butterfly','nymphalidae','nectar','forests','6');
+
+
+class Reptile extends Vertebrates {
     constructor(name, species, diet, habitat, numLegs) {
-      super(name, species, diet, habitat);
-      this.numLegs = numLegs
+        super(name, species, diet, habitat);
+        this.name = name;
+        this.species = species;
+        this.diet = diet;
+        this.habitat = habitat;
+        this.numLegs = numLegs
+        this.bodytemp= ('cold blooded');
     }
-  }
+}
+// Instantiating object from the subclass above.
+const turtle = new Reptile('Turtle','testudine','vegetables','ocean','4');
 
-// Instantiating Objects from the subclasses above.
 
-const arthropoda = new Arthropoda ('Arthropoda');
-const fish = new Fish ('Fish');
-const amphibia = new Amphibia('Amphibia');
-const reptile = new Reptile('Reptile');
-const aves = new Aves('AVES');
-const mammal = new Mammal('Mammal');
+const animalia = [butterfly, tilapia, toad, turtle, dove, cat];
 
-const animalia = [arthropoda, fish, amphibia, reptile, aves, mammal];
+console.log(`This shows a simple classification for Kingdom Animalia:`);
+console.log(animalia);
--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # AnimalKingdom
-Classification of Animal Kingdom using Object oriented programming in Javascript
+Classification of Animal Kingdom using Object oriented programming in Javascript. 
+
+This code models a simple classification of the animal kingdom. 
+It follows the four basic principles of Object Oriented Programming. These four principles are:
+
+• Encapsulation • Abstraction • Inheritance • Polymorphism.
+
+It is broadly divided into the following phyla: 
+ Arthropoda, Aves, Mammal, Reptile, Amphibian and Fish.
+
+ An abstract base class, KingdomAnimalia, is shown. It houses attributes inherited by the subclasses, 'Vertebrates' and 'Invertebrates'. Each of 6 phyla are then shown to inherit attributes from either of the subclasses. Classes were made for each one and objects we're instantiated from them.
+
+ To run this code, please clone this repository. Or you can always copy the copy and paste it directly on your IDE of choice.
+Ensure you have nodejs installed and configured.
+Enter this command in your terminal: Node AnimalKingdom.js.
+If done correctly, this should be the output:
+
+This shows a simple classification for Kingdom Animalia:
+[
+  Arthropoda {
+    name: 'Butterfly',
+    species: 'nymphalidae',
+    diet: 'nectar',
+    habitat: 'forests',
+    backbone: 'have no backbone',
+    numLegs: '6',
+    bodytemp: 'cold blooded'
+  },
+  Fish {
+    name: 'Tilapia',
+    species: 'cichlidae',
+    diet: 'plankton',
+    habitat: 'freshwater',
+    backbone: 'have no backbone',
+    numFins: '7',
+    bodytemp: 'cold blooded'
+  },
+  Amphibia {
+    name: 'Toad',
+    species: 'bufonidae',
+    diet: 'insects',
+    habitat: 'moist land',
+    backbone: 'have a backbone',
+    numLegs: '4',
+    bodytemp: 'cold blooded'
+  },
+  Reptile {
+    name: 'Turtle',
+    species: 'testudine',
+    diet: 'vegetables',
+    habitat: 'ocean',
+    backbone: 'have a backbone',
+    numLegs: '4',
+    bodytemp: 'cold blooded'
+  },
+  Aves {
+    name: 'Dove',
+    species: 'columbidae',
+    diet: 'grains',
+    habitat: 'arboreal',
+    backbone: 'have a backbone',
+    numWings: '2',
+    bodytemp: 'warm blooded'
+  },
+  Mammal {
+    name: 'Cat',
+    species: 'felidae',
+    diet: 'canned food',
+    habitat: 'land',
+    backbone: 'have a backbone',
+    numLegs: '4',
+    bodytemp: 'warm blooded'
+  }
+]
